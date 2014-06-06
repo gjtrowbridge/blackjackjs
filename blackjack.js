ce8512a21792cf1401fb68dd38893f940b160698
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-06-06 11:57:37 -0700

    Blackjack basically complete, beginning to add browser interface

--- blackjack.js
+++ blackjack.js
@@ -50,22 +50,46 @@ Deck.prototype.initialize = function() {
   }
 }
 
-//Shuffles the deck
+//Chooses a random index from among the remaining cards
+Deck.prototype._getRandomIndex = function() {
+  return Math.floor(Math.random() * this.cards.length);
+}
+
+//Shuffles the deck (can also shuffle when some cards have been dealt already)
 Deck.prototype.shuffle = function(num) {
-  var getRandomIndex = function() {
-    return Math.floor(Math.random() * 52);
-  }
   if (num === undefined) { 
     num = 10000;
   }
   for (var i=0; i<num; i++) {
-    var ind1 = getRandomIndex();
-    var ind2 = getRandomIndex();
+    var ind1 = this._getRandomIndex();
+    var ind2 = this._getRandomIndex();
 
     var temp = this.cards[ind1];
     this.cards[ind1] = this.cards[ind2];
     this.cards[ind2] = temp;
   }
+}
+
+//Removes and returns the card at the front of the deck
+Deck.prototype.popFirstCard = function() {
+  return this.cards.shift();
+}
+
+//Gets a random card from the deck
+//This allows you to "play" without needing to shuffle
+Deck.prototype.popRandomCard = function() {
+  //Gets a random index
+  var index = this._getRandomIndex();
+  
+  //Gets the card at that index
+  var card = this.cards[index];
+
+  //Removes the card from the "cards" array
+  this.cards[index] = this.cards[0];
+  this.cards.shift();
+
+  //Returns chosen card
+  return card;
 }
 
 //Player class
@@ -77,21 +101,56 @@ var Player = function(firstName) {
 //Hand class
 var Hand = function() {
   this.cards = [];
+  this.bust = false;
+  this.total = 0;
 }
 
-//Gets the total value of the current hand
-Hand.prototype.getTotal = function() {
-  var total = 0;
+//Pushes a card to the hand
+Hand.prototype.pushCard = function(card) {
+  this.cards.push(card);
+  this._updateInternalProperties();
+}
+
+//Saves the total value of the current hand
+//also saves a "bust" boolean
+Hand.prototype._updateInternalProperties = function() {
+  this.total = 0;
+  var aces = 0;
+
+  //Counts up combined value of all cards
   for (var i=0; i<this.cards.length; i++) {
     var card = this.cards[i];
-    total += card;
+    this.total += card.val;
+    
+    //Counts # of aces
+    if (card.val === 1) {
+      aces = 0;
+    }
   }
-  return total;
+
+  //Changes ace values to 11 when appropriate
+  while ((this.total + 10 <= 21)&&(aces > 0)) {
+    aces--;
+    this.total+=10;
+  }
+
+  if (this.total > 21) {
+    this.bust = true;
+  } else {
+    this.bust = false;
+  }
 }
+
+
 
 //Game class--accepts arbitrary number of player objects
 var Game = function() {
+  //Creates and shuffles a new deck
   this.deck = new Deck();
+  deck.shuffle();
+  this.dealer = new Player('dealer');
+
+  //Saves all players in this game (passed in as arguments)
   this.players = [];
   for (var i=0; i<arguments.length; i++) {
     var player = arguments[i];
@@ -99,8 +158,62 @@ var Game = function() {
   }
 }
 
+//Deals hands to all players
+Game.prototype.startGame = function() {
+  //Deal two cards to all players
+  for (var i=1; i<=2; i++) {
+    this.dealCard(this.dealer)
+    this._eachPlayer(function(player) {
+      this.dealCard(player);
+    });
+  }
+}
+
+//Calls an iterator on each player in the game
+Game.prototype._eachPlayer = function(iterator) {
+  for (var i=0; i<this.players.length; i++) {
+    iterator.call(this, this.players[i]);
+  }
+}
+
+//Handles dealer behavior before the game ends,
+//then determines winners
+Game.prototype.endGame = function() {
+  while (this.dealer.hand.total < 17) {
+    this.dealCard(this.dealer);
+  }
+  this._eachPlayer(function(player) {
+    if (this.dealer.hand.bust) {
+      console.log("Dealer is bust, " + player.firstName + " wins.");
+    } else if (player.hand.bust) {
+      console.log(player.firstName + " is bust, and loses to the dealer.");
+    } else if (player.hand.total > this.dealer.hand.total) {
+      console.log(player.firstName + " beats the dealer with a score of " + 
+        player.hand.total.toString() + " vs " + this.dealer.hand.total.toString() + ".")
+    } else if (player.hand.total === this.dealer.hand.total) {
+      console.log(player.firstName + " ties with the dealer with a score of " +
+        player.hand.total.toString() + " vs " + this.dealer.hand.total.toString() + ".")
+    } else {
+      console.log(player.firstName + " loses to the dealer with a score of " +
+        player.hand.total.toString() + " vs " + this.dealer.hand.total.toString() + ".")
+    }
+  });
+}
+
+//Deals a card from the game deck to a specific player
+Game.prototype.dealCard = function(player) {
+  player.hand.pushCard(this.deck.popFirstCard());
+  return player.hand.total;
+}
+
+//Checks whether the given player won the game
+Game.prototype.playerWon = function() {
+  
+}
+
 var deck = new Deck();
 deck.shuffle();
 deck.inspect();
 var greg = new Player('greg');
-var dealer = new Player('dealer');
+var game = new Game(greg);
+game.startGame();
