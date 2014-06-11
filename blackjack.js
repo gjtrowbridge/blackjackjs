82d8361a2cd9ef2c5aa84660119bf62a9a98d54d
Hack Reactor Students <accounts@hackreactor.com>
2014-06-10 20:58:31 -0700

    Added front end interface, updated some code to make things more usable.

--- blackjack.js
+++ blackjack.js
@@ -2,6 +2,7 @@
 var Card = function(suit, val) {
   this.suit = suit;
   this.val = val;
+  this.numVal = Math.min(val, 10);
 
   if (val === 1) {
     this.stringVal = 'Ace';
@@ -14,6 +15,8 @@ var Card = function(suit, val) {
   } else {
     this.stringVal = val.toString();
   }
+
+
 };
 
 //Converts the card to a human-readable string
@@ -122,11 +125,11 @@ Hand.prototype._updateInternalProperties = function() {
   //Counts up combined value of all cards
   for (var i=0; i<this.cards.length; i++) {
     var card = this.cards[i];
-    this.total += card.val;
+    this.total += card.numVal;
 
     //Counts # of aces
-    if (card.val === 1) {
-      aces = 0;
+    if (card.numVal === 1) {
+      aces++;
     }
   }
 
@@ -144,17 +147,24 @@ Hand.prototype._updateInternalProperties = function() {
 };
 
 //Game class--accepts arbitrary number of player objects
-var Game = function() {
+var Game = function(numOfPlayers) {
   //Creates and shuffles a new deck
   this.deck = new Deck();
   this.deck.shuffle();
+  this.gameOver = false;
+  this.summary = 'Game has not started.'
+
+  //Creates a dealer and the specified # of players
+  this.players = [];
+
+  //Creates and stores the dealer
   this.dealer = new Player('dealer');
   this.dealer.dealer = true;
+  this.players.push(this.dealer);
 
-  //Saves all players in this game (passed in as arguments)
-  this.players = [];
-  for (var i=0; i<arguments.length; i++) {
-    var player = arguments[i];
+  //Creates and stores the players
+  for (var i=1; i<=numOfPlayers; i++) {
+    var player = new Player('player' + i.toString());
     this.players.push(player);
   }
 };
@@ -168,11 +178,14 @@ Game.prototype.startGame = function() {
       this.dealCard(player);
     });
   }
+
+  //Saves the current state of the game
+  this.summary = 'Dealer shows ' + this.dealer.hand.total + '. Hit?';
 };
 
 //Calls an iterator on each player in the game
 Game.prototype._eachPlayer = function(iterator) {
-  for (var i=0; i<this.players.length; i++) {
+  for (var i=1; i<this.players.length; i++) {
     iterator.call(this, this.players[i]);
   }
 };
@@ -180,29 +193,38 @@ Game.prototype._eachPlayer = function(iterator) {
 //Handles dealer behavior before the game ends,
 //then determines winners
 Game.prototype.endGame = function() {
+  this.gameOver = true;
   while (this.dealer.hand.total < 17) {
     this.dealCard(this.dealer);
   }
   this._eachPlayer(function(player) {
-    if (this.dealer.hand.bust) {
-      console.log('Dealer is bust, ' + player.firstName + ' wins.');
-    } else if (player.hand.bust) {
-      console.log(player.firstName + ' is bust, and loses to the dealer.');
+    if (player.hand.bust) {
+      this.summary = player.firstName + ' is bust, and loses to the dealer.';
+    } else if (this.dealer.hand.bust) {
+      this.summary = 'Dealer is bust, ' + player.firstName + ' wins.';
     } else if (player.hand.total > this.dealer.hand.total) {
-      console.log(player.firstName + ' beats the dealer with a score of ' +
-        player.hand.total.toString() + ' vs ' + this.dealer.hand.total.toString() + '.');
+      this.summary = player.firstName + ' beats the dealer with a score of ' +
+        player.hand.total.toString() + ' vs ' + this.dealer.hand.total.toString() + '.';
     } else if (player.hand.total === this.dealer.hand.total) {
-      console.log(player.firstName + ' ties with the dealer with a score of ' +
-        player.hand.total.toString() + ' vs ' + this.dealer.hand.total.toString() + '.');
+      this.summary = player.firstName + ' ties with the dealer with a score of ' +
+        player.hand.total.toString() + ' vs ' + this.dealer.hand.total.toString() + '.';
     } else {
-      console.log(player.firstName + ' loses to the dealer with a score of ' +
-        player.hand.total.toString() + ' vs ' + this.dealer.hand.total.toString() + '.');
+      this.summary = player.firstName + ' loses to the dealer with a score of ' +
+        player.hand.total.toString() + ' vs ' + this.dealer.hand.total.toString() + '.';
     }
   });
+  console.log(this.summary);
 };
 
 //Deals a card from the game deck to a specific player
+//Accepts either a player object or the index of the player
+//If no arguments passed, defaults to the first player
 Game.prototype.dealCard = function(player) {
+  if (typeof(player) === 'number') {
+    player = this.players[player];
+  } else if (player === undefined) {
+    player = this.players[1];
+  }
   player.hand.pushCard(this.deck.popFirstCard());
   return player.hand.total;
 };
--- init.js
+++ init.js
@@ -1,33 +1,84 @@
 $(document).ready(function() {
-  var game;
-  var player;
 
-  var updateCards = function(player) {
-    if (player.dealer) {
-      var $cards = $('#dealer').find('li.card');
-      $cards.eq(0).html('Hidden');
-      $cards.eq(1).html(player.hand.cards[1].toString());
-    } else {
-      var $cards = $('#player1').find('li.card');
-      for (var i=0; i<$cards.length; i++) {
-        $cards.eq(i).html(player.hand.cards[i].toString());
+  //updates the cards html
+  var updateGameHtml = function() {
+    clearAll();
+    outputCards();
+    outputResults();
+  };
+
+  //updates the html to reflect the current game progress
+  var outputResults = function() {
+    var $results = $('#results').find('p').html('');
+    $results.html(window.game.summary);
+  };
+
+  //Clears the cards html
+  var clearAll = function() {
+    $('#dealer').find('ul.hand').html('');
+    $('#player1').find('ul.hand').html('');
+    $('#results').find('p').html('');
+  };
+  var outputCards = function() {
+
+    //Outputs cards for each player in the game
+    for (var i=0; i<window.game.players.length; i++) {
+      var player = window.game.players[i];
+
+      //Finds the output list
+      var $cardsList = $('#' + player.firstName).find('ul.hand');
+      console.log($cardsList);
+
+      //Deals cards to the appropriate location
+      for (var j=0; j<player.hand.cards.length; j++) {
+        //The card to deal
+        var card = player.hand.cards[j];
+
+        //Creates a list element to insert the card
+        var $cardHtml = $('<li>');
+
+        //Doesn't show the dealer's first card until the game is over
+        if (player.dealer && !window.game.gameOver && j===0) {
+          $cardHtml.html('hidden')
+          $cardsList.append($cardHtml);
+        //Otherwise inserts the card's suit and value to the element
+        } else {
+          $cardHtml.html(card.toString());
+        }
+
+        //Display the card element
+        $cardsList.append($cardHtml);
       }
     }
   };
 
+  //Runs immediately
+  window.game = new Game(1);
+  window.game.startGame();
+  updateGameHtml();
+
+
+  //Creates the game and deals down cards
   $('.startGame').on('click', function(event) {
-    player = new Player('Player 1');
-    game = new Game(player);
-    game.startGame();
-    console.log(game);
-    updateCards(player);
-    updateCards(game.dealer);
+    window.game = new Game(1);
+    window.game.startGame();
+    updateGameHtml();
   });
+
+  //Deals a card to the player
   $('.hit').on('click', function(event) {
-    game.dealCard(player);
-    updateCards(player);
+    //The first player has index 0
+    window.game.dealCard();
+
+    //Updates the game
+    updateGameHtml();
   });
+
+  //Outputs the end-of-game results
   $('.stay').on('click', function(event) {
-    game.endGame();
+    window.game.endGame();
+
+    updateGameHtml();
   });
+
 });
